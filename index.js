7237198d161598f4a25d196a116b871a909c9270
mahgah189 <123992408+mahgah189@users.noreply.github.com>
2024-01-29 16:58:40 -0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,17 +1,45 @@
 const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"];
+const charactersLettersOnly = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
 
 let pwOneEl = document.getElementById("pw1");
 let pwTwoEl = document.getElementById("pw2");
 let pwLength = document.getElementById("length-el");
 let errorMessage = document.getElementById("error-msg");
+let successMessage = document.getElementById("success-msg");
+let toggleText = document.getElementById("toggle-state");
+let copyPasswordOneButton = document.getElementById("pw1-copy");
+let copyPasswordTwoButton = document.getElementById("pw2-copy");
 
+let toggleState = false;
 let pwLengthCache;
 
 function passwordLength() {
     pwLengthCache = pwLength.value;
-    pwLength.value = "";
     errorMessage.textContent = "";
+
+    copyPasswordOneButton.textContent = "Copy password one";
+    copyPasswordTwoButton.textContent = "Copy password two";
+    pwOneEl.textContent = "";
+    pwTwoEl.textContent = "";
+
+    if (pwLengthCache === undefined || pwLengthCache === "") {
+        errorMessage.textContent = "Please set a password length";
+        successMessage.textContent = "";
+    } else {
+        successMessage.textContent = "Password length set.";
+        errorMessage.textContent = "";
+    }
+}
+
+function toggle() {
+    if (toggleState === true) {
+        toggleState = false;
+        toggleText.textContent = "Symbols & numbers are enabled."
+    } else {
+        toggleState = true;
+        toggleText.textContent = "Symbols & numbers are disabled."
+    }
 }
 
 function passwordGenerator() {
@@ -20,7 +48,10 @@ function passwordGenerator() {
     let passwordOne = "";
     let passwordTwo = "";
 
-    console.log(pwLengthCache);
+    pwLength.value = "";
+    successMessage.textContent = "";
+    copyPasswordOneButton.textContent = "Copy password one";
+    copyPasswordTwoButton.textContent = "Copy password two";
 
     if (pwLengthCache === undefined || pwLengthCache === "") {
         errorMessage.textContent = "Please set a password length";
@@ -28,9 +59,16 @@ function passwordGenerator() {
     }
 
     // create a for loop to generate each character (up to 15 characters)
-    for (let i = 0; i < pwLengthCache; i++) {
-        passwordOne += characters[Math.floor(Math.random() * characters.length)];
-        passwordTwo += characters[Math.floor(Math.random() * characters.length)];
+    if (toggleState === false) {
+        for (let i = 0; i < pwLengthCache; i++) {
+            passwordOne += characters[Math.floor(Math.random() * characters.length)];
+            passwordTwo += characters[Math.floor(Math.random() * characters.length)];
+        }
+    } else {
+        for (let i = 0; i < pwLengthCache; i++) {
+            passwordOne += charactersLettersOnly[Math.floor(Math.random() * charactersLettersOnly.length)];
+            passwordTwo += charactersLettersOnly[Math.floor(Math.random() * charactersLettersOnly.length)];
+        }
     }
 
     // push each password to its respective field
@@ -40,10 +78,12 @@ function passwordGenerator() {
 
 function copyPasswordOne() {
     navigator.clipboard.writeText(pwOneEl.textContent);
-    alert("Copied password one: " + pwOneEl.textContent);
+    copyPasswordOneButton.textContent = "Copied!"
+    copyPasswordTwoButton.textContent = "Copy password two";
 }
 
 function copyPasswordTwo() {
     navigator.clipboard.writeText(pwTwoEl.textContent);
-    alert("Copied password two: " + pwTwoEl.textContent);
+    copyPasswordTwoButton.textContent = "Copied!"
+    copyPasswordOneButton.textContent = "Copy password one";
 }
